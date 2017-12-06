03e42a65ce87c64d8e596105c1f8e38f9de9e0b7
huynhvp <vietphi3892@gmail.com>
2017-12-06 10:27:46 +0100

    ProjE: neural network vers. for KGMiner

--- ProjE_fact_checking.py
+++ ProjE_fact_checking.py
@@ -27,14 +27,14 @@ class ProjE:
         with open(".csv", 'r', encoding='utf-8') as f_triple:
            self.__train_predpath = 0 # need to update
            self.__hrt_triples = 0 # need to update
-        self.__n_predicate = self.__train_predpath.shape[1]
+        self.n_predicate = self.__train_predpath.shape[1]
         print("N_TRAIN_TRIPLES: %d" % self.__train_predpath.shape[0])
         
 
         bound = 6 / math.sqrt(embed_dim)
 
         with tf.device('/cpu'):
-            self.__pred_embedding = tf.get_variable("pred_embedding", [self.__n_predicate, embed_dim],
+            self.__pred_embedding = tf.get_variable("pred_embedding", [self.n_predicate, embed_dim],
                                                    initializer=tf.random_uniform_initializer(minval=-bound,
                                                                                              maxval=bound,
                                                                                              seed=345))
@@ -66,14 +66,49 @@ class ProjE:
             pred_list, pred_weight = inputs
 
 
-            pred_res = tf.nn.dropout(tf.tanh(tf.matmul(pred_list,
-                                                          self.__pred_embedding) + self.__pred_bias),
-                                        self.__dropout)
+            pred_res = tf.matmul(pred_list, self.__pred_embedding) + self.__pred_bias
+                                      
         
             self.pred_softmax = pred_res_softmax = self.sampled_softmax(hrt_res, pred_weight)
 
             pred_loss = -tf.reduce_sum(
-                tf.log(tf.clip_by_value(pred_res_softmax, 1e-10, 1.0)) * hr_tlist_weight)
+                tf.log(tf.clip_by_value(pred_res_softmax, 1e-10, 1.0)) * pred_weight)
 
           
             return pred_loss
+
+    def test(self, inputs, scope=None):
+        with tf.variable_scope(scope or type(self).__name__) as scp:
+            scp.reuse_variables()
+
+            pred_res = tf.matmul(pred_list, self.__pred_embedding) + self.__pred_bias
+            pred_res *= -1
+            np.exp(pred_res, pred_res)
+            pred_res += 1
+            np.reciprocal(pred_res, pred_res)
+            
+            return np.vstack([1 - pred_res, pred_res]).T
+
+
+def train_ops(model: ProjE, learning_rate=0.1, optimizer_str='gradient', regularizer_weight=1.0):
+    with tf.device('/cpu'):
+        pred_input = tf.placeholder(tf.int32, [None, model.n_predicate])
+        pred_weight = tf.placeholder(tf.float32, [None])
+
+
+        loss = model.train([pred_input, pred_weight],
+                           regularizer_weight=regularizer_weight)
+        if optimizer_str == 'gradient':
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        elif optimizer_str == 'rms':
+            optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
+        elif optimizer_str == 'adam':
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        else:
+            raise NotImplementedError("Does not support %s optimizer" % optimizer_str)
+
+        grads = optimizer.compute_gradients(loss, model.trainable_variables)
+
+        op_train = optimizer.apply_gradients(grads)
+
+        return pred_input, pred_weight, loss, op_train
