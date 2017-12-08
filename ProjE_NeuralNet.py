b20747d4d4fb38e414fa5a8558179a44b2829db7
huynhvp <vietphi3892@gmail.com>
2017-12-08 12:56:45 +0100

    ProjE MLP

--- ProjE_NeuralNet.py
+++ ProjE_NeuralNet.py
@@ -36,13 +36,13 @@ n_predicate = train_predpath.shape[1]-1
 print("N_TRAIN_TRIPLES: %d" % train_predpath.shape[0])
 
 # Parameters
-learning_rate = 0.01
+learning_rate = 0.1
 training_epochs = 100
 batch_size = 100
 display_step = 1
 
 # Network Parameters
-n_hidden_1 = 200 # 1st layer number of neurons
+n_hidden_1 = 100 # 1st layer number of neurons
 n_hidden_2 = 1 # 2nd layer number of neurons
 n_input =  n_predicate# MNIST data input (img shape: 28*28)
 n_classes = 2 # MNIST total classes (0-9 digits)
