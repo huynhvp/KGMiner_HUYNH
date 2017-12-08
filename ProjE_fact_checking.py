08d03763dc12ff906878b9f77d5eb55c3a02d5a4
huynhvp <vietphi3892@gmail.com>
2017-12-08 10:39:57 +0100

    Update ProjE

--- ProjE_fact_checking.py
+++ ProjE_fact_checking.py
@@ -99,12 +99,12 @@ class ProjE:
             pred_res = tf.matmul(tf.matmul(pred_list, self.__pred_embedding), self.__pred_bias)
                                       
             #pred_res_sigmoid = tf.sigmoid(pred_res)
-            self.pred_softmax = pred_res_softmax = self.sampled_softmax(pred_res, pred_weight)
-
-            pred_loss = -tf.reduce_sum(
-                tf.log(tf.clip_by_value(pred_res_softmax, 1e-10, 1.0)) * pred_weight)
+            #self.pred_softmax = pred_res_softmax = self.sampled_softmax(pred_res, pred_weight)
+            #self.pred_softmax = pred_res_softmax 
+            #pred_loss = -tf.reduce_sum(
+            #    tf.log(tf.clip_by_value(pred_res_softmax, 1e-10, 1.0)) * pred_weight)
            #     + tf.log(tf.clip_by_value(1 - pred_res_sigmoid, 1e-10, 1.0)) * (1-pred_weight))
-            
+            pred_loss = tf.nn.softmax_cross_entropy_with_logits(pred_res, pred_weight)
             self.regularizer_loss = regularizer_loss = tf.reduce_sum(
                 tf.abs(self.__pred_embedding)) 
           
