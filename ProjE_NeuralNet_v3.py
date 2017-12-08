5a5762c0e9b83a750cda3146dbb78369ea0645b1
huynhvp <vietphi3892@gmail.com>
2017-12-08 20:42:45 +0100

    2-Layer NN v3

--- ProjE_NeuralNet_v3.py
+++ ProjE_NeuralNet_v3.py
@@ -70,11 +70,11 @@ biases = {
 # Create model
 def multilayer_perceptron(x):
     # Hidden fully connected layer with 256 neurons
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.matmul(x, weights['h1'])
     # Hidden fully connected layer with 256 neurons
     #layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     # Output fully connected layer with a neuron for each class
-    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+    out_layer = tf.matmul(layer_1, weights['out']) 
     return out_layer
 
 # Construct model
