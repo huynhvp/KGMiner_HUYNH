07288b99d9c3840a4430c8d22b0fbcdb3e462bf9
huynhvp <vietphi3892@gmail.com>
2017-12-08 23:17:51 +0100

    2-Layer NN v3

--- ProjE_NeuralNet_v3.py
+++ ProjE_NeuralNet_v3.py
@@ -37,12 +37,12 @@ print("N_TRAIN_TRIPLES: %d" % train_predpath.shape[0])
 
 # Parameters
 learning_rate = 0.01
-training_epochs = 50
+training_epochs = 500
 batch_size = 100
 display_step = 1
 
 # Network Parameters
-n_hidden_1 = 75 # 1st layer number of neurons
+n_hidden_1 = 200 # 1st layer number of neurons
 #n_hidden_2 = 1 # 2nd layer number of neurons
 n_input =  n_predicate# MNIST data input (img shape: 28*28)
 n_classes = 2 # MNIST total classes (0-9 digits)
