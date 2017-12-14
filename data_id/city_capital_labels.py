d0893684c0b4d6579e0f9d1f41b25d57d3e1bc4e
huynhvp <vietphi3892@gmail.com>
2017-12-15 00:58:33 +0100

    Extract labels test and training set for city-capital

---
+++ data_id/city_capital_labels.py
@@ -0,0 +1,32 @@
+file1_names = open('./data_id/city_capitals', 'r')
+ids = open('./data_id/city_capitals_labels.tsv', 'w')
+
+
+file1_name = file1_names.readline()
+file1_name = file1_names.readline()
+state_id = file1_name.split(',')[0]
+city_id = file1_name.split(',')[1]
+is_capital = file1_name.split(',')[2].split('\n')[0]
+
+while file1_name != "":
+    file1_found = False
+    file2_found = False
+    str_to_write = "\t"
+    nodes = open('./data/infobox.nodes', 'r')
+    node = nodes.readline()
+    while node != "" and (not file1_found or not file2_found):
+        node_info = node.split('\t')
+        if node_info[0] == city_id:
+            file2_found = True
+            str_to_write = str_to_write + node_info[0]
+        if node_info[0] == state_id:
+            file1_found = True
+            str_to_write = node_info[0] + str_to_write
+        node = nodes.readline()
+    str_to_write += "\t" + ("1" if is_capital=="TRUE" else "-1")
+    print "writing " + str_to_write 
+    ids.write(str_to_write + '\n')
+    state_id = file1_name.split(',')[0]
+    city_id = file1_name.split(',')[1]
+    is_capital = file1_name.split(',')[2].split('\n')[0]
+ids.close()
